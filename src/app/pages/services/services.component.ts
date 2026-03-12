d53748827c1b6005b28e495136e3bbe9717e4370
AmeeraMaxwellos <amimaxwell8441@gmail.com>
2026-03-12 10:10:51 +0300

    updated

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { SeoService } from '../../service/seo.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
